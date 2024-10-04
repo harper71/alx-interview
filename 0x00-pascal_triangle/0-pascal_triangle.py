ea749eb1ec1f83cba1a651982dacf900b6d2d7ea
harper71 <princeazuka99@gmail.com>
2024-10-04 03:30:50 +0100

    commit changes

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """this is to create a pascals triangle
+
+    Args:
+        n (integer): the number of triangles
+
+    Returns:
+        list: a list of the values
+    """
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(1, i):
+
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+        triangle.append(row)
+    return triangle
